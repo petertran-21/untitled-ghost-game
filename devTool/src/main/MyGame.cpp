dd77d2260a7485c9065984afb22364bbb8d294c7
michelle <lovering.michelle1@gmail.com>
2020-02-24 14:09:14 -0500

    select box works for one sprite

--- devTool/src/main/MyGame.cpp
+++ devTool/src/main/MyGame.cpp
@@ -29,50 +29,78 @@ MyGame::~MyGame(){
 
 void MyGame::update(set<SDL_Scancode> pressedKeys){
 
-		if (Game::mouse->leftClick) {
-			if(character->position.x > Game::mouse->selectBoxX && Game::mouse->selectBoxX > character->position.x + character->width
-			&& character->position.y > Game::mouse->selectBoxY && Game::mouse->selectBoxY > character->position.y + character->height) {
+	// X, Y is location of start, X2, Y2 is location of current
+	if (Game::mouse->leftClick) {
+		if(character->position.x < Game::mouse->selectBoxX && Game::mouse->selectBoxX < character->position.x + character->width
+		&& character->position.y < Game::mouse->selectBoxY && Game::mouse->selectBoxY < character->position.y + character->height
+		&& Game::mouse->selectBoxX2 == Game::mouse->selectBoxX && Game::mouse->selectBoxY2 == Game::mouse->selectBoxY) {
+			character->selected = true;
+		} else if (!(Game::mouse->selectBoxX2 == Game::mouse->selectBoxX) && !(Game::mouse->selectBoxY2 == Game::mouse->selectBoxY)) {
+			int topRX,topRY,botLX,botLY;
+			if(Game::mouse->selectBoxX < Game::mouse->selectBoxX2 && Game::mouse->selectBoxY < Game::mouse->selectBoxY2) { // is the current mouse location the topR of the rect ?
+				// start is top left, current is bottom right
+				topRX = Game::mouse->selectBoxX2;
+				topRY = Game::mouse->selectBoxY;
+				botLX = Game::mouse->selectBoxX;
+				botLY = Game::mouse->selectBoxY2;
+			} else if(Game::mouse->selectBoxX > Game::mouse->selectBoxX2 && Game::mouse->selectBoxY < Game::mouse->selectBoxY2) { // is the current mouse location the topR of the rect ?
+				// start is top right, current is bottom left
+				topRX = Game::mouse->selectBoxX;
+				topRY = Game::mouse->selectBoxY;
+				botLX = Game::mouse->selectBoxX2;
+				botLY = Game::mouse->selectBoxY2;
+			} else if(Game::mouse->selectBoxX < Game::mouse->selectBoxX2 && Game::mouse->selectBoxY > Game::mouse->selectBoxY2) { // is the current mouse location the topR of the rect ?
+				// start is bottom left, , current is top right
+				topRX = Game::mouse->selectBoxX;
+				topRY = Game::mouse->selectBoxY;
+				botLX = Game::mouse->selectBoxX2;
+				botLY = Game::mouse->selectBoxY2;
+			} else if(Game::mouse->selectBoxX > Game::mouse->selectBoxX2 && Game::mouse->selectBoxY > Game::mouse->selectBoxY2) { // is the current mouse location the topR of the rect ?
+				// start is bottom right, , current is top left
+				topRX = Game::mouse->selectBoxX;
+				topRY = Game::mouse->selectBoxY2;
+				botLX = Game::mouse->selectBoxX2;
+				botLY = Game::mouse->selectBoxY;
+			}
+
+			if(character->position.x < topRX &&
+				 botLX < character->position.x + character->width &&
+				 character->position.y + character->height > topRY &&
+				 botLY > character->position.y) {
+							character->selected = true;
+			}
+		} else {
 				character->selected = false;
 			}
-			else if(character->position.x > Game::mouse->selectBoxX ||
-							Game::mouse->selectBoxX2 > character->position.x + character->width ||
-							character->position.y + character->height < Game::mouse->selectBoxY2 ||
-							Game::mouse->selectBoxY < character->position.y) {
-								character->selected = false;
-			}
+	}
 
-			else {
-				character->selected = true;
+	if (character->selected) {
+		if (pressedKeys.find(SDL_SCANCODE_RIGHT) != pressedKeys.end()) {
+			character->position.x += Game::cellSize;
+		}
+		if (pressedKeys.find(SDL_SCANCODE_LEFT) != pressedKeys.end()) {
+			character->position.x -= Game::cellSize;
+		}
+		if (pressedKeys.find(SDL_SCANCODE_DOWN) != pressedKeys.end()) {
+			character->position.y += Game::cellSize;
+		}
+		if (pressedKeys.find(SDL_SCANCODE_UP) != pressedKeys.end()) {
+			character->position.y -= Game::cellSize;
+		}
+		if (pressedKeys.find(SDL_SCANCODE_Q) != pressedKeys.end()) {
+			character->width = Game::cellSize * Game::scale;
+			character->height = Game::cellSize * Game::scale;
+			Game::scale++;
+		}
+		if (pressedKeys.find(SDL_SCANCODE_W) != pressedKeys.end()) {
+			character->width = Game::cellSize * Game::scale;
+			character->height = Game::cellSize * Game::scale;
+			Game::scale--;
+			if (Game::scale < 1) {
+				Game::scale = 1;
 			}
 		}
-
-		if (character->selected) {
-			if (pressedKeys.find(SDL_SCANCODE_RIGHT) != pressedKeys.end()) {
-				character->position.x += Game::cellSize;
-			}
-			if (pressedKeys.find(SDL_SCANCODE_LEFT) != pressedKeys.end()) {
-				character->position.x -= Game::cellSize;
-			}
-			if (pressedKeys.find(SDL_SCANCODE_DOWN) != pressedKeys.end()) {
-				character->position.y += Game::cellSize;
-			}
-			if (pressedKeys.find(SDL_SCANCODE_UP) != pressedKeys.end()) {
-				character->position.y -= Game::cellSize;
-			}
-			if (pressedKeys.find(SDL_SCANCODE_Q) != pressedKeys.end()) {
-				character->width = Game::cellSize * Game::scale;
-				character->height = Game::cellSize * Game::scale;
-				Game::scale++;
-			}
-			if (pressedKeys.find(SDL_SCANCODE_W) != pressedKeys.end()) {
-				character->width = Game::cellSize * Game::scale;
-				character->height = Game::cellSize * Game::scale;
-				Game::scale--;
-				if (Game::scale < 1) {
-					Game::scale = 1;
-				}
-			}
-		}
+	}
 	Game::update(pressedKeys);
 }
 
