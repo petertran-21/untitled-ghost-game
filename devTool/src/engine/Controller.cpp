ad5cb5597c6081362de8bbc168fd9f975e957902
Bradley Knaysi <30380036+bradknaysi@users.noreply.github.com>
2020-03-05 14:16:55 -0500

    Added base Controller class
    Controller class is a wrapper around the `SDL_Joystick` class so we can add additional functionality and abstract our interactions with the xbox controller events.

---
+++ devTool/src/engine/Controller.cpp
@@ -0,0 +1,37 @@
+#include "Controller.h"
+
+/**
+ * Author: Bradley Knaysi
+ * File: Controller.cpp
+ * Created: March 5th, 2020
+ */
+
+Controller::Controller() {
+
+	// *** PART OF THE CONTROLLER TUTORIAL ***
+	if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) ) {
+		printf( "Warning: Linear texture filtering not enabled!" );
+	}
+
+	// Controller must be connected via bluetooth to computer before running ./bin/myGame
+	printf("SDL_NumJoysticks() = %d\n", SDL_NumJoysticks());
+
+	// Check for joystick
+	if(SDL_NumJoysticks() > 0) {
+		printf( "Joystick connected! \n" );
+
+		// Load joystick
+		joystick = SDL_JoystickOpen(0);
+		if(joystick == NULL) {
+			printf( "Warning: Unable to open game controller! SDL Error: %s\n", SDL_GetError() );
+		}
+	}
+};
+
+Controller::~Controller() {
+    joystick = NULL;
+};
+
+SDL_Joystick* Controller::getJoystick() {
+    return joystick;
+}
---
+++ devTool/src/engine/Controller.h
@@ -0,0 +1,29 @@
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
+#include <SDL2/SDL.h>
+#include <iostream>
+
+using namespace std;
+
+/**
+ * Author: Bradley Knaysi
+ * File: Controller.h
+ * Created: March 5th, 2020
+ */
+class Controller {
+
+    public:
+
+        Controller();
+        ~Controller();
+
+        SDL_Joystick* getJoystick();
+
+    private:
+
+        SDL_Joystick* joystick = NULL;
+        const int JOYSTICK_DEAD_ZONE = 5000;
+};
+
+#endif
