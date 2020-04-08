aa7357f83666d1f285f37146faee90042d0dcac0
michelle <lovering.michelle1@gmail.com>
2020-04-07 20:22:30 -0400

    commit

--- src/main/MyGame.cpp
+++ src/main/MyGame.cpp
@@ -7,56 +7,18 @@
 using namespace std;
 
 MyGame::MyGame() : Game(1000, 1000){
-	//------------Work from the SFX/Level Team
-	camera = new Camera();
-	scene_1 = new Scene();
-
-	scene_1->loadScene("./resources/scenes/beachEntrance.json");
-
-	this->addChild(camera);
-	camera->addChild(scene_1);
-	//-----------------------------------------
-
-	//-------------Character Programming Demo
 	instance = this;
 
 	allSprites = new DisplayObjectContainer();
 	instance->addChild(allSprites);
-	container = new DisplayObjectContainer();
-
-	// uncomment following line to check that collision boxes for objects are identical to drawing
-	allSprites->addChild(container);
-
-	collisionSystem = new CollisionSystem();
-	displayTreeDisp = new EventDispatcher();
-	DOAdded = new DOAddedEvent(displayTreeDisp, container);
-	DORemoved = new DORemovedEvent(displayTreeDisp, container);
-	displayTreeDisp->addEventListener(collisionSystem, DOAddedEvent::DO_ADDED);
-	displayTreeDisp->addEventListener(collisionSystem, DORemovedEvent::DO_REMOVED);
 
 
-
-	// npc = new NPCPyromancer(container, allSprites);
-	// npc->position.x += 300;
-	// allSprites->addChild(npc);
-	// container->addChild(npc);
-
-	// NPCArcher* npc2 = new NPCArcher(container, allSprites);
-	// allSprites->addChild(npc2);
-	// container->addChild(npc2);
-
-	// player = new Ghost();
-	// allSprites->addChild(player);
-	// container->addChild(player);
-
-	// DOAdded->addChildCalled(container);
-	// DOAdded->checkCondition();
+	player = new AnimatedSprite
 
 }
 
 MyGame::~MyGame(){
-	delete scene_1;
-	delete camera;
+
 }
 
 void MyGame::update(set<SDL_Scancode> pressedKeys, Controller::JoystickState currState){
