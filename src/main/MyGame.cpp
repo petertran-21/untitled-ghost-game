6227698720d419a76c81efad26f2de757687b049
michelle <lovering.michelle1@gmail.com>
2020-04-07 20:19:28 -0400

    first

--- src/main/MyGame.cpp
+++ src/main/MyGame.cpp
@@ -17,7 +17,7 @@ MyGame::MyGame() : Game(1000, 1000){
 	camera->addChild(scene_1);
 	//-----------------------------------------
 
-	//-------------Character Programming Demo 
+	//-------------Character Programming Demo
 	instance = this;
 
 	allSprites = new DisplayObjectContainer();
@@ -26,7 +26,7 @@ MyGame::MyGame() : Game(1000, 1000){
 
 	// uncomment following line to check that collision boxes for objects are identical to drawing
 	allSprites->addChild(container);
-	
+
 	collisionSystem = new CollisionSystem();
 	displayTreeDisp = new EventDispatcher();
 	DOAdded = new DOAddedEvent(displayTreeDisp, container);
@@ -34,17 +34,9 @@ MyGame::MyGame() : Game(1000, 1000){
 	displayTreeDisp->addEventListener(collisionSystem, DOAddedEvent::DO_ADDED);
 	displayTreeDisp->addEventListener(collisionSystem, DORemovedEvent::DO_REMOVED);
 
-	// Shrub* s = new Shrub(container);
-	// allSprites->addChild(s);
-	// container->addChild(s);
 
-	ItemPouch* p = new ItemPouch(container);
-	p->position.x = 900;
-	p->position.y = 900;
-	allSprites->addChild(p);
-	container->addChild(p);
 
-	// npc = new NPCPyromancer(container, allSprites); 
+	// npc = new NPCPyromancer(container, allSprites);
 	// npc->position.x += 300;
 	// allSprites->addChild(npc);
 	// container->addChild(npc);
@@ -60,73 +52,6 @@ MyGame::MyGame() : Game(1000, 1000){
 	// DOAdded->addChildCalled(container);
 	// DOAdded->checkCondition();
 
-	Shrub* s = new Shrub(container);
-	allSprites->addChild(s);
-	container->addChild(s);
-
-	BreakableWall *b = new BreakableWall(container);
-	allSprites->addChild(b);
-	container->addChild(b);
-	b->position.x += 200;
-	b->position.y += 200;
-
-	Valve * v = new Valve(container);
-	allSprites->addChild(v);
-	container->addChild(v);
-	v->position.x += 500;
-	v->position.y += 300;
-
-	WaterJet * w = new WaterJet(container, allSprites);
-	allSprites->addChild(w);
-	container->addChild(w);
-	v->add_jet(w);
-	w->position.x += 700;
-	w->position.y += 100;
-
-	Crab * c = new Crab();
-	allSprites->addChild(c);
-	container->addChild(c);
-	c->position.y += 600;
-	c->position.x += 600;
-
-	npc = new NPCPyromancer(container, allSprites); 
-	npc->position.x += 300;
-	allSprites->addChild(npc);
-	container->addChild(npc);
-
-	NPCOperator* npc2 = new NPCOperator(container, allSprites);
-	npc2->position.y += 500;
-	allSprites->addChild(npc2);
-	container->addChild(npc2);
-
-	NPCExcavator* npc3 = new NPCExcavator(container, allSprites);
-	allSprites->addChild(npc3);
-	container->addChild(npc3);
-
-	NPCCollector* npc4 = new NPCCollector(container, allSprites);
-	npc4->position.x += 300;
-	npc4->position.y += 500;
-	allSprites->addChild(npc4);
-	container->addChild(npc4);
-
-	player = new Ghost();
-	allSprites->addChild(player);
-	container->addChild(player);
-
-	DOAdded->addChildCalled(container);
-	DOAdded->checkCondition();
-	//-----------------------------------------
-	// TODO, SFX will add later
-
-	// selectionMenuTest = new SelectionMenu();
-	// selectionMenuTest->addToMenu("Save");
-	// selectionMenuTest->addToMenu("Load");
-	// selectionMenuTest->addToMenu("Quit");
-	// container->addChild(selectionMenuTest);
-
-	// checklistTest = new Checklist();
-	// checklistTest->addEntry("Change Scene");
-	// container->addChild(checklistTest);
 }
 
 MyGame::~MyGame(){
