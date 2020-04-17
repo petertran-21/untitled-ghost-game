93fce8cdcab010d5dd7a6b3588c86f0bdc96c44b
AustinB21 <austin@bunting.us>
2020-04-16 22:38:06 -0400

    Fixed convertScene.py and finished beach scene conversion

--- resources/convertScene.py
+++ resources/convertScene.py
@@ -1,9 +1,11 @@
 import json
 
-filepath = 'beachRoom6.json'
-with open(filepath) as json_file:
+filepath = 'scenes/beachBossD.json'
+with open(filepath, 'r') as json_file:
     data = json.load(json_file)
     for gameObject in data["sprites"]:
         gameObject["subtype"] = 2
 
-    json.dump(data, json_file)
+
+with open (filepath, 'w') as json_file:
+    json.dump(data, json_file, indent=4)
