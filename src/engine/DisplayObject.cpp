a3a18d4a851fcbfa68a348870a42f475b9adff5d
Bradley Knaysi <30380036+bradknaysi@users.noreply.github.com>
2020-04-17 14:32:57 -0400

    Hitboxes around DisplayObject now hidden

--- src/engine/DisplayObject.cpp
+++ src/engine/DisplayObject.cpp
@@ -163,7 +163,7 @@ void DisplayObject::createHitbox(){
 	int w = (int)distance(hitBoxPoints.at(0), hitBoxPoints.at(1));
 	int h = (int)distance(hitBoxPoints.at(1), hitBoxPoints.at(2));
 	SDL_Rect rect = { hitBoxPoints.at(0).x, hitBoxPoints.at(0).y, w, h};
-	// SDL_SetTextureAlphaMod(hitBox->curTexture, 0);
+	SDL_SetTextureAlphaMod(hitBox->curTexture, 0);
 	SDL_RenderCopyEx(Game::renderer, hitBox->curTexture, NULL, &rect, calculateRotation(hitBoxPoints.at(0), hitBoxPoints.at(1)), &corner, SDL_FLIP_NONE);
 }
 
