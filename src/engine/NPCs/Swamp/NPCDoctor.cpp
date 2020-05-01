b692b001fee77a21b18b893f19037c1f511bb6ea
Bradley Knaysi <30380036+bradknaysi@users.noreply.github.com>
2020-05-01 01:34:49 -0400

    fixed look left bug

--- src/engine/NPCs/Swamp/NPCDoctor.cpp
+++ src/engine/NPCs/Swamp/NPCDoctor.cpp
@@ -7,7 +7,7 @@ using namespace std;
 NPCDoctor::NPCDoctor(DisplayObjectContainer* container, DisplayObjectContainer* allSprites, vector<DisplayObject*> &passedInventory) : MainNPC(){
     this->addAnimation("./resources/npcs/doctor/", "doctor_idle", 1, 1, true, "idle");
     this->addAnimation("./resources/npcs/doctor/", "doctor_forward", 6, 10, true, "forward");
-    this->addAnimation("./resources/npcs/doctor/", "doctor_left", 6, 10, true, "left'");
+    this->addAnimation("./resources/npcs/doctor/", "doctor_left", 6, 10, true, "left");
     this->addAnimation("./resources/npcs/doctor/", "doctor_right", 6, 10, true, "right");
     this->addAnimation("./resources/npcs/doctor/", "doctor_back", 6, 10, true, "back");
 
