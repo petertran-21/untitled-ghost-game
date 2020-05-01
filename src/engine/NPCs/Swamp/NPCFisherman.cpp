1e4420b9e5ed206ab654183bc1c1d2f258363cc3
Bradley Knaysi <30380036+bradknaysi@users.noreply.github.com>
2020-05-01 01:45:23 -0400

    fixed look left bug p2

--- src/engine/NPCs/Swamp/NPCFisherman.cpp
+++ src/engine/NPCs/Swamp/NPCFisherman.cpp
@@ -7,7 +7,7 @@ using namespace std;
 NPCFisherman::NPCFisherman(DisplayObjectContainer* container, DisplayObjectContainer* allSprites, vector<DisplayObject*> &passedInventory) : MainNPC(){
     this->addAnimation("./resources/npcs/fisherman/", "fisherman_idle", 1, 1, true, "idle");
     this->addAnimation("./resources/npcs/fisherman/", "fisherman_forward", 6, 10, true, "forward");
-    this->addAnimation("./resources/npcs/fisherman/", "fisherman_left", 6, 10, true, "left'");
+    this->addAnimation("./resources/npcs/fisherman/", "fisherman_left", 6, 10, true, "left");
     this->addAnimation("./resources/npcs/fisherman/", "fisherman_right", 6, 10, true, "right");
     this->addAnimation("./resources/npcs/fisherman/", "fisherman_back", 6, 10, true, "back");
     this->addAnimation("./resources/npcs/fisherman/", "fisherman_boat", 1, 1, false, "onboat");
