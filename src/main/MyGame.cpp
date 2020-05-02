046252b2ce445a49710ab63696dee654240c283c
michelle <lovering.michelle1@gmail.com>
2020-05-01 20:01:10 -0400

    done

--- src/main/MyGame.cpp
+++ src/main/MyGame.cpp
@@ -23,13 +23,58 @@ MyGame::MyGame() : Game(1000, 1000)
 	startScreen->height = 1000;
 	startScreen->play("main");
 
-	loreScreen = new AnimatedSprite();
-	loreScreen->addAnimation("./resources/start/","lore",1,1,true,"lore");
-	loreScreen->width = 1000;
-	loreScreen->height = 700;
-	loreScreen->position.y = 60;
-	loreScreen->alpha = 0;
-	loreScreen->play("lore");
+	// stone henge
+	loreScreen1 = new AnimatedSprite();
+	loreScreen1->addAnimation("./resources/start/","lore",1,1,true,"lore");
+	loreScreen1->width = 1000;
+	loreScreen1->height = 700;
+	loreScreen1->position.y = 60;
+	loreScreen1->alpha = 0;
+	loreScreen1->play("lore");
+
+	// godly symbols
+	loreScreen2 = new AnimatedSprite();
+	loreScreen2->addAnimation("./resources/start/","symbols",1,1,true,"symbols");
+	loreScreen2->width = 1000;
+	loreScreen2->height = 700;
+	loreScreen2->position.y = 60;
+	loreScreen2->alpha = 0;
+	loreScreen2->play("symbols");
+
+	// horn
+	loreScreen3 = new AnimatedSprite();
+	loreScreen3->addAnimation("./resources/start/","horn",6,36,true,"horn");
+	loreScreen3->width = 1000;
+	loreScreen3->height = 1000;
+	loreScreen3->alpha = 0;
+	loreScreen3->play("horn");
+
+	// grendel monster
+	loreScreen4 = new AnimatedSprite();
+	loreScreen4->addAnimation("./resources/start/","grendelMonster",4,36,true,"monster");
+	loreScreen4->width = 1000;
+	loreScreen4->height = 1000;
+	loreScreen4->alpha = 0;
+	loreScreen4->play("monster");
+
+	// grendel ghost
+	loreScreen5 = new AnimatedSprite();
+	loreScreen5->addAnimation("./resources/start/","grendelGhost",4,36,true,"ghost");
+	loreScreen5->width = 800;
+	loreScreen5->height = 900;
+	loreScreen5->position.x = 150;
+	loreScreen5->alpha = 0;
+	loreScreen5->play("ghost");
+
+	//grendel ghost colored
+	loreScreen6 = new AnimatedSprite();
+	loreScreen6->addAnimation("./resources/start/","gren",1,1,true,"ghostColor");
+	loreScreen6->width = 450;
+	loreScreen6->height = 700;
+	loreScreen6->position.x = 250;
+	loreScreen6->position.y = 60;
+	loreScreen6->alpha = 0;
+	loreScreen6->play("ghostColor");
 
 	mapTutorial = new AnimatedSprite();
 	mapTutorial->addAnimation("./resources/map/", "gameMap", 1, 1, true, "plain");
@@ -92,32 +137,175 @@ void MyGame::update(set<SDL_Scancode> pressedKeys, Controller::JoystickState cur
 				fadeStart = false;
 			}
 		}
-		if(!startScreenVisible && !fadeLoreIn && !loreScreenVisible && !fadeLoreOut && !mapScreenVisible) {
+		if(!startScreenVisible && !fadeLoreIn1 && !lore1ScreenVisible && !fadeLoreOut1 && !screen1Complete) {
 			allSprites->removeImmediateChild(startScreen);
-			allSprites->addChild(loreScreen);
-			fadeLoreIn = true;
+			allSprites->addChild(loreScreen1);
+			fadeLoreIn1 = true;
 		}
-		if(fadeLoreIn && !loreScreenVisible) {
-			loreScreen->alpha = loreScreen->alpha + 5;
-			if(loreScreen->alpha >= 255) {
-				loreScreen->alpha = 255;
-				loreScreenVisible = true;
-				fadeLoreIn = false;
+		if(fadeLoreIn1 && !lore1ScreenVisible) {
+			loreScreen1->alpha = loreScreen1->alpha + 5;
+			if(loreScreen1->alpha >= 255) {
+				loreScreen1->alpha = 255;
+				lore1ScreenVisible = true;
+				fadeLoreIn1 = false;
 			}
 		}
-		if(pressedKeys.find(SDL_SCANCODE_RIGHT) != pressedKeys.end() && loreScreenVisible) {
-			fadeLoreOut = true;
+		if(pressedKeys.size() > 0 && lore1ScreenVisible) {
+			fadeLoreOut1 = true;
 		}
-		if(fadeLoreOut && loreScreenVisible) {
-			loreScreen->alpha = loreScreen->alpha - 5;
-			if(loreScreen->alpha <= 0) {
-				loreScreen->alpha = 0;
-				loreScreenVisible = false;
-				fadeLoreOut = false;
+		if(fadeLoreOut1 && lore1ScreenVisible) {
+			loreScreen1->alpha = loreScreen1->alpha - 5;
+			if(loreScreen1->alpha <= 0) {
+				loreScreen1->alpha = 0;
+				lore1ScreenVisible = false;
+				fadeLoreOut1 = false;
+				screen1Complete = true;
 			}
 		}
-		if(!startScreenVisible && !fadeLoreIn && !fadeLoreOut && !loreScreenVisible && !mapScreenVisible) {
-			allSprites->removeImmediateChild(loreScreen);
+
+		//
+		if(screen1Complete && !lore1ScreenVisible && !fadeLoreIn2 && !lore2ScreenVisible && !fadeLoreOut2 && !screen2Complete) {
+			allSprites->removeImmediateChild(loreScreen1);
+			allSprites->addChild(loreScreen2);
+			fadeLoreIn2 = true;
+		}
+		if(fadeLoreIn2 && !lore2ScreenVisible) {
+			loreScreen2->alpha = loreScreen2->alpha + 5;
+			if(loreScreen2->alpha >= 255) {
+				loreScreen2->alpha = 255;
+				lore2ScreenVisible = true;
+				fadeLoreIn2 = false;
+			}
+		}
+		if(pressedKeys.size() > 0 && lore2ScreenVisible) {
+			fadeLoreOut2 = true;
+		}
+		if(fadeLoreOut2 && lore2ScreenVisible) {
+			loreScreen2->alpha = loreScreen2->alpha - 5;
+			if(loreScreen2->alpha <= 0) {
+				loreScreen2->alpha = 0;
+				lore2ScreenVisible = false;
+				fadeLoreOut2 = false;
+				screen2Complete = true;
+			}
+		}
+
+
+		//
+		if(screen2Complete && !lore2ScreenVisible && !fadeLoreIn3 && !lore3ScreenVisible && !fadeLoreOut3 && !screen3Complete) {
+			allSprites->removeImmediateChild(loreScreen2);
+			allSprites->addChild(loreScreen3);
+			fadeLoreIn3 = true;
+		}
+		if(fadeLoreIn3 && !lore3ScreenVisible) {
+			loreScreen3->alpha = loreScreen3->alpha + 5;
+			if(loreScreen3->alpha >= 255) {
+				loreScreen3->alpha = 255;
+				lore3ScreenVisible = true;
+				fadeLoreIn3 = false;
+			}
+		}
+		if(pressedKeys.size() > 0 && lore3ScreenVisible) {
+			fadeLoreOut3 = true;
+		}
+		if(fadeLoreOut3 && lore3ScreenVisible) {
+			loreScreen3->alpha = loreScreen3->alpha - 5;
+			if(loreScreen3->alpha <= 0) {
+				loreScreen3->alpha = 0;
+				lore3ScreenVisible = false;
+				fadeLoreOut3 = false;
+				screen3Complete = true;
+			}
+		}
+		//
+
+		//
+		if(screen3Complete && !lore3ScreenVisible && !fadeLoreIn4 && !lore4ScreenVisible && !fadeLoreOut4 && !screen4Complete) {
+			allSprites->removeImmediateChild(loreScreen3);
+			allSprites->addChild(loreScreen4);
+			fadeLoreIn4 = true;
+		}
+		if(fadeLoreIn4 && !lore4ScreenVisible) {
+			loreScreen4->alpha = loreScreen4->alpha + 5;
+			if(loreScreen4->alpha >= 255) {
+				loreScreen4->alpha = 255;
+				lore4ScreenVisible = true;
+				fadeLoreIn4 = false;
+			}
+		}
+		if(pressedKeys.size() > 0 && lore4ScreenVisible) {
+			fadeLoreOut4 = true;
+		}
+		if(fadeLoreOut4 && lore4ScreenVisible) {
+			loreScreen4->alpha = loreScreen4->alpha - 5;
+			if(loreScreen4->alpha <= 0) {
+				loreScreen4->alpha = 0;
+				lore4ScreenVisible = false;
+				fadeLoreOut4 = false;
+				screen4Complete = true;
+			}
+		}
+		//
+
+		//
+		if(screen4Complete && !lore4ScreenVisible && !fadeLoreIn5 && !lore5ScreenVisible && !fadeLoreOut5 && !screen5Complete) {
+			allSprites->removeImmediateChild(loreScreen4);
+			allSprites->addChild(loreScreen5);
+			fadeLoreIn5 = true;
+		}
+		if(fadeLoreIn5 && !lore5ScreenVisible) {
+			loreScreen5->alpha = loreScreen5->alpha + 5;
+			if(loreScreen5->alpha >= 255) {
+				loreScreen5->alpha = 255;
+				lore5ScreenVisible = true;
+				fadeLoreIn5 = false;
+			}
+		}
+		if(pressedKeys.size() > 0 && lore5ScreenVisible) {
+			fadeLoreOut5 = true;
+		}
+		if(fadeLoreOut5 && lore5ScreenVisible) {
+			loreScreen5->alpha = loreScreen5->alpha - 5;
+			if(loreScreen5->alpha <= 0) {
+				loreScreen5->alpha = 0;
+				lore5ScreenVisible = false;
+				fadeLoreOut5 = false;
+				screen5Complete = true;
+			}
+		}
+		//
+
+		//
+		if(screen5Complete && !lore5ScreenVisible && !fadeLoreIn6 && !lore6ScreenVisible && !fadeLoreOut6 && !screen6Complete) {
+			allSprites->removeImmediateChild(loreScreen5);
+			allSprites->addChild(loreScreen6);
+			fadeLoreIn6 = true;
+		}
+		if(fadeLoreIn6 && !lore6ScreenVisible) {
+			loreScreen6->alpha = loreScreen6->alpha + 5;
+			if(loreScreen6->alpha >= 255) {
+				loreScreen6->alpha = 255;
+				lore6ScreenVisible = true;
+				fadeLoreIn6 = false;
+			}
+		}
+		if(pressedKeys.size() > 0 && lore6ScreenVisible) {
+			fadeLoreOut6 = true;
+		}
+		if(fadeLoreOut6 && lore6ScreenVisible) {
+			loreScreen6->alpha = loreScreen6->alpha - 5;
+			if(loreScreen6->alpha <= 0) {
+				loreScreen6->alpha = 0;
+				lore6ScreenVisible = false;
+				fadeLoreOut6 = false;
+				screen6Complete = true;
+			}
+		}
+		//
+
+		//
+		if(!mapScreenVisible && screen1Complete && screen2Complete && screen3Complete && screen4Complete && screen5Complete && screen6Complete) {
+			allSprites->removeImmediateChild(loreScreen6);
 			allSprites->addChild(mapTutorial);
 			mapScreenVisible = true;
 			mapTutorial->play("tutorial");
--- src/main/MyGame.h
+++ src/main/MyGame.h
@@ -78,14 +78,40 @@ private:
 
 	// intro stuff
 	AnimatedSprite* startScreen;
-	AnimatedSprite* loreScreen;
+	AnimatedSprite* loreScreen1;
+	AnimatedSprite* loreScreen2;
+	AnimatedSprite* loreScreen3;
+	AnimatedSprite* loreScreen4;
+	AnimatedSprite* loreScreen5;
+	AnimatedSprite* loreScreen6;
 	AnimatedSprite* mapTutorial;
 	bool playStartSequence = true;
 	bool startScreenVisible = true;
 	bool fadeStart = false;
-	bool loreScreenVisible = false;
-	bool fadeLoreIn = false;
-	bool fadeLoreOut = false;
+	bool lore1ScreenVisible = false;
+	bool lore2ScreenVisible = false;
+	bool lore3ScreenVisible = false;
+	bool lore4ScreenVisible = false;
+	bool lore5ScreenVisible = false;
+	bool lore6ScreenVisible = false;
+	bool fadeLoreIn1 = false;
+	bool fadeLoreOut1 = false;
+	bool fadeLoreIn2 = false;
+	bool fadeLoreOut2 = false;
+	bool fadeLoreIn3 = false;
+	bool fadeLoreOut3 = false;
+	bool fadeLoreIn4 = false;
+	bool fadeLoreOut4 = false;
+	bool fadeLoreIn5 = false;
+	bool fadeLoreOut5 = false;
+	bool fadeLoreIn6 = false;
+	bool fadeLoreOut6 = false;
+	bool screen1Complete = false;
+	bool screen2Complete = false;
+	bool screen3Complete = false;
+	bool screen4Complete = false;
+	bool screen5Complete = false;
+	bool screen6Complete = false;
 	bool mapScreenVisible = false;
 	bool townHighlighted = false;
 
